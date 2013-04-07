8d4ca500668093e9fd36005ae8cbb33023e7d63b
alkise <alkise@AlkiseNotebook>
2013-04-07 10:18:13 +0600

    run on 1099 port

--- src/ru/alkise/manager/FTPManagerServer.java
+++ src/ru/alkise/manager/FTPManagerServer.java
@@ -7,6 +7,7 @@ package ru.alkise.manager;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import ru.alkise.manager.model.ManagerModelRemote;
 import ru.alkise.manager.model.ManagerModelRemoteIntf;
 import ru.alkise.manager.model.itemlist.ItemListIntf;
@@ -22,6 +23,7 @@ public class FTPManagerServer {
             ItemListIntf fromList = new FileItemList("/home/alkise/Pictures");
             ItemListIntf toList = new FileItemList("/home/alkise/Documents");
             ManagerModelRemoteIntf model = new ManagerModelRemote(fromList, toList);
+            LocateRegistry.createRegistry(1099);
             Naming.rebind("ManagerModel", model);
             System.out.println("Server started");
         } catch (RemoteException | MalformedURLException ex) {
